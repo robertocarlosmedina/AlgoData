d600c980a94309f1286e65962a0693e919658cb7
Roberto-04-dev <true4321king@gmail.com>
2021-03-10 21:33:09 -0100

    Starting whit the ordination algorithms display.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame 
-from pygame import *
+from pygame.locals import *
 from pygame.constants import K_KP_ENTER
 from src.start import Start as st
 from src.ordination import Ordination as odt
@@ -21,7 +21,6 @@ while keep_going:
     
     if pygame.key.get_pressed()[K_KP_ENTER]:
         keep_going = not keep_going
-    
     
     current_layout = link[current_layout].run(screen, screen_size)
     pygame.display.update()
--- src/ordination/__init__.py
+++ src/ordination/__init__.py
@@ -1,31 +1,41 @@
 import pygame
+from pygame.locals import *
 from src.colors import color, rgbColor
 
 class Ordination():
     pygame.init()
     def __init__(self):
         self.header_box = pygame.Rect(30, 20,580 , 40)
-        self.font = pygame.font.Font("montserrat-font/MontserratMedium-nRxlJ.ttf", 20)
+        self.font = pygame.font.SysFont("arial", 20)
         self.font.set_bold(True)
         self.sort_algorithms = ["Insertion Sort","Selection Sort", "Bubble Sort","Quicksort", "Merge Sort","Shell Sort", "Hybrid Sort"]
         self.active = self.sort_algorithms[0]
-    
+        
     def buttonsDisplay(self, screen):
         y = 70
-        x1, y1, = 430, 77
+        x1, y1, = 430, 80
+        box_dim=(190, 40)
+        
         for algorithm in self.sort_algorithms:
-            button_box = pygame.Rect(420, y, 190, 40)
+            mouse_pos = pygame.mouse.get_pos()
+            size = pygame.font.Font.size(self.font, algorithm)
+            button_box = pygame.Rect(420, y, box_dim[0], box_dim[1])
             if(self.active == algorithm):
                 pygame.draw.rect(screen, color.green.value, button_box)
                 line = self.font.render(algorithm, True, color.black.value)
             else:
                 pygame.draw.rect(screen, color.grey.value, button_box, 2)
                 line = self.font.render(algorithm, True, color.white.value)
+            
+            # checking if the algorithms choice
+            click = pygame.mouse.get_pressed()
+            if mouse_pos[0]in range(x1,x1+box_dim[0]) and mouse_pos[1] in range(y1,y1+box_dim[1]) and click[0] == 1:
+                self.active = algorithm
 
-            # size = pygame.font.Font.size(self.font, algorithm)
             screen.blit(line, (x1, y1))
             y += 45
             y1 += 45
+        
     
     def run(self,screen, screen_size):
         screen.fill(color.black.value)

85116b2dbcc6c1ffbcc305eadf55e652d52073aa
Roberto-04-dev <true4321king@gmail.com>
2021-03-10 10:07:45 -0100

    Adding the intro.

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame 
 from pygame import *
+from pygame.constants import K_KP_ENTER
 
 screen_size = (640, 420)
 colors = {"blue":(0, 0, 255), "white":(255, 255, 255), "green":(0, 255, 0), "black":(35,35,35)}
 screen = pygame.display.set_mode((screen_size))
 pygame.display.set_caption('AlgoDataStuct')
+size = 40
 
-def start():
-
+def start(size):
+    box_pos = [((280, 170),"green"),((320, 170), "blue"), ((280, 210), "blue"),((320, 210), "green")]
     screen.fill(colors["black"])
-    pygame.draw.rect(screen, colors["green"], pygame.Rect(0, 0, 40, 40))
-    pygame.draw.rect(screen, colors["blue"], pygame.Rect(320, 0, 40, 40))
-    pygame.draw.rect(screen, colors["blue"], pygame.Rect(0, 210, 40, 40))
-    pygame.draw.rect(screen, colors["green"], pygame.Rect(320, 210, 40, 40))
+
+    [pygame.draw.rect(screen, colors[info[1]], pygame.Rect(info[0][0], info[0][1], size, size)) for info in box_pos]
+
+    if size>4:
+        return size - 0.01
+    else: 
+        return 0
+    
+
+
 
 keep_going = True
 
@@ -20,7 +28,11 @@ while keep_going:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
+    
+    if pygame.key.get_pressed()[K_KP_ENTER]:
+        exit()
 
-    start()
+
+    size = start(size)
     
     pygame.display.update()

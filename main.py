0d7790515681bd0d793f25ca3257918912347080
Roberto-04-dev <true4321king@gmail.com>
2021-03-10 11:24:15 -0100

    Adding the intro.

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ screen_size = (640, 420)
 colors = {"blue":(0, 0, 255), "white":(255, 255, 255), "green":(0, 255, 0), "black":(35,35,35)}
 screen = pygame.display.set_mode((screen_size))
 pygame.display.set_caption('AlgoDataStuct')
-size = 40
+
+## variables to the start display
+global size = 40
+global box_info = []
+global y, x == 320, 210
 
 def start(size):
+    
+    for i in range(4):
+        box_info.append(1)
     box_pos = [((280, 170),"green"),((320, 170), "blue"), ((280, 210), "blue"),((320, 210), "green")]
     screen.fill(colors["black"])
 
@@ -18,9 +25,6 @@ def start(size):
         return size - 0.01
     else: 
         return 0
-    
-
-
 
 keep_going = True
 
